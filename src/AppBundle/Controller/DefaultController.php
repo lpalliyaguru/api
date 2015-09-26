96ee9719c4a3c3ecdb233fcc0b04fd7d22940630
Manoj Lasantha <manoj.lasantha@insead.edu>
2015-09-27 07:43:23 +0800

    Cleaned the default controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,11 +15,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $serializer = $this->get('serializer');
-        $commentManager = $this->get('manager.comment');
-        $comment = $commentManager->getOneById(1);
 
-        $data = $serializer->serialize($comment, 'json' , SerializationContext::create()->setVersion('2.2.0'));
-        return new JsonResponse($data);
+        return new JsonResponse(array('success' => true));
     }
 }
